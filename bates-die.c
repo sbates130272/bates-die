017f067d5e4f3cc1499ef58822b2f849393c269d
Stephen Bates <sbates@raithlin.com>
2016-07-19 12:45:40 +0000

    Progress on die simulation.

--- bates-die.c
+++ bates-die.c
@@ -14,27 +14,78 @@ struct bates_die_t {
 	unsigned long seed;
 	unsigned ngames;
 	unsigned nsides;
-	unsigned nwins;
+	unsigned nwin;
+
+	unsigned *wins;
+	unsigned games;
+	unsigned npdf;
 };
 
 static const struct bates_die_t defaults = {
 	.seed       = 0,
 	.ngames     = 100,
 	.nsides     = 6,
-	.nwins      = 5,
+	.nwin       = 5,
 };
 
 static const struct argconfig_commandline_options command_line_options[] = {
 	{"seed", "NUM", CFG_LONG, &defaults.seed, required_argument,
-			"number of io operations to do"},
+			"seed for random number generator"},
 	{"n", "NUM", CFG_LONG_SUFFIX, &defaults.ngames, required_argument,
 			"number of games to play"},
 	{"s", "NUM", CFG_LONG, &defaults.nsides, required_argument,
 			"number of sides on the die"},
-	{"w", "NUM", CFG_LONG_SUFFIX, &defaults.nwins, required_argument,
-			"number of sides needsed to win the game"},
+	{"w", "NUM", CFG_LONG_SUFFIX, &defaults.nwin, required_argument,
+			"number of rools of winning sides needsed to win the game"},
 	{0}
 };
+
+static void setup(struct bates_die_t *bates_die)
+{
+	bates_die->games = 0;
+	bates_die->npdf  = (bates_die->nwin-1)*bates_die->nsides-
+		bates_die->nwin;
+	bates_die->wins  = calloc(bates_die->npdf, sizeof(*bates_die->wins));
+	gettimeofday(&bates_die->starttime, NULL);
+}
+
+static void teardown(struct bates_die_t *bates_die)
+{
+	free(bates_die->wins);
+}
+
+static void report(struct bates_die_t *bates_die)
+{
+	fprintf(stdout,"Bates Die Game Results\n");
+	fprintf(stdout,"----------------------\n\n");
+	fprintf(stdout,"nsides = %3d : nwin = %4d : ngames = %6d\n",
+		bates_die->nsides, bates_die->nwin, bates_die->ngames);
+	fprintf(stdout,"Results after %4d games (%d sec)\n",
+		bates_die->games, 0);
+	for (unsigned i=0; i<bates_die->npdf; i++)
+		fprintf(stdout,"pdf[%04d] = %4d\n", i, bates_die->wins[i]);
+}
+
+static void play(struct bates_die_t *bates_die)
+{
+	unsigned roll, rolls = 0, *score;
+
+	score = calloc(bates_die->nsides, sizeof(bates_die->nsides));
+	while(1)
+	{
+		roll = rand() % (bates_die->nsides);
+		printf("%d\n", roll);
+		score[roll]++;
+		rolls++;
+		if (score[roll]==bates_die->nwin){
+			bates_die->wins[rolls]++;
+			bates_die->games++;
+			break;
+		}
+	}
+	free(score);
+}
+	
 	
 int main(int argc, char **argv)
 {
@@ -48,7 +99,10 @@ int main(int argc, char **argv)
 		bates_die.seed = time(NULL);
 	srand(bates_die.seed);
 
-	gettimeofday(&bates_die.starttime, NULL);
+	setup(&bates_die);
+	play(&bates_die);
+	report(&bates_die);
+	teardown(&bates_die);
 	
 	return 0;
 }
